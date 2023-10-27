9f9c887b84bc72e1ede334dd61d183930dfc4e61
ldphat99 <ldphat99@gmail.com>
2023-10-27 23:07:29 +0700

    ADD: user model

---
+++ models/userModel.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose")
+const validator = require("validator")
+const bcrypt = require("bcryptjs")
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+  },
+  role: {
+    type: String,
+    enum: ["admin","manager", "user"],
+    default: "user",
+  },
+  address: {
+    type:String,
+    default: ""
+  },
+  phone: {
+    type: String,
+    default: ""
+  }
+})
+
+// Hashed the password before saving the user into database
+UserSchema.pre("save", async function () {
+  // console.log(this.modifiedPaths());
+  // console.log(this.isModified("name"));
+  if (!this.isModified("password")) return
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+// Compare password
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password)
+  return isMatch
+} 
+
+module.exports = mongoose.model("User", UserSchema)
