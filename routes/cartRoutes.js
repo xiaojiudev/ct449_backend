cc80ecc8bc9ace144fb5d9983c83e51e1dbbc5d1
ldphat99 <ldphat99@gmail.com>
2023-10-29 21:17:13 +0700

    ADD: cart route

---
+++ routes/cartRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    addToCart,
+    removeProductFromCart,
+    clearCart,
+    getUserCart,
+} = require("../controllers/cartController");
+
+const {
+    authenticateUser,
+    authorizePermissions,
+} = require("../middleware/authentication");
+
+router
+    .route("/")
+    .get([authenticateUser], getUserCart)
+    .post([authenticateUser], addToCart)
+    .delete([authenticateUser], removeProductFromCart)
+
+router
+    .route("/clear")
+    .delete([authenticateUser], clearCart)
+
+module.exports = router
