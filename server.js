b5ac719c3328007b0b77fbbb608ef130fa38dd76
ldphat99 <ldphat99@gmail.com>
2023-10-29 21:21:33 +0700

    ADD: main app

---
+++ server.js
@@ -0,0 +1,54 @@
+require("express-async-errors")
+
+const express = require("express")
+const app = express()
+
+// Rest of the packages
+const morgan = require("morgan") //HTTP request logger middleware
+const cookieParser = require("cookie-parser")
+const fileUpload = require("express-fileupload")
+// Require Database
+const connectDB = require("./db/connect")
+// Require Routers
+const authRouter = require("./routes/authRoutes")
+const userRouter = require("./routes/userRoutes")
+const productRouter = require("./routes/productRoutes")
+const cartRouter = require("./routes/cartRoutes")
+const reviewRouter = require("./routes/reviewRoutes")
+const orderRouter = require("./routes/orderRoutes")
+// Require Middleware
+const notFoundMiddleware = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
+
+// Invoke Extra packages
+app.use(morgan("tiny"))
+app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
+app.use(express.static("./public"))
+
+
+// Invoke Routers
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/products", productRouter)
+app.use("/api/v1/carts", cartRouter)
+app.use("/api/v1/reviews", reviewRouter)
+app.use("/api/v1/orders", orderRouter)
+// Invoke Middleware
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 8080
+const start = async () => {
+  try {
+    // Connect database
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, () =>
+      console.log(`🚀 Server is listening on port ${port}...`)
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
